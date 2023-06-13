e87825c5b5075bd0db293d0410193a165e02572a
BuildTools <unconfigured@null.spigotmc.org>
2023-06-12 23:02:53 -0500

    thingys

---
+++ voyager.py
@@ -0,0 +1,13 @@
+from voyager import Voyager
+
+# You can also use mc_port instead of azure_login, but azure_login is highly recommended
+mc_port = 57043
+openai_api_key = "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"
+
+voyager = Voyager(
+    mc_port=mc_port,
+    openai_api_key=openai_api_key,
+    ckpt_dir="ckpt_test",
+)
+
+voyager.learn()
