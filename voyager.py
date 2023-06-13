a2bae97a0ce935beb4b58a36edcd5dd2c2b1d2b9
BuildTools <unconfigured@null.spigotmc.org>
2023-06-12 23:13:52 -0500

    Update voyager.py

--- voyager.py
+++ voyager.py
@@ -2,7 +2,7 @@ from voyager import Voyager
 
 # You can also use mc_port instead of azure_login, but azure_login is highly recommended
 mc_port = input("Enter Minecraft port: ")
-openai_api_key = "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"
+openai_api_key = input("Enter OpenAI API key: ")
 
 voyager = Voyager(
     mc_port=mc_port,
